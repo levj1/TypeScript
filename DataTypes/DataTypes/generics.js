5d3b5e929c9c60398b18bf4202788d4f3580f240
levj1 <levj1@yahoo.com>
2017-04-19 15:53:34 -0400

    generics

---
+++ DataTypes/DataTypes/generics.js
@@ -0,0 +1,6 @@
+function identity(arg) {
+    return arg;
+}
+var output = identity(5);
+console.log(output);
+//# sourceMappingURL=generics.js.map
---
+++ DataTypes/DataTypes/generics.ts
@@ -0,0 +1,8 @@
+﻿
+function identity<T>(arg: T): T {
+    return arg;
+}
+
+let output = identity(5);
+
+console.log(output);
